ba1ee0ff8413449acf6a41dc89d688d010b1d0df
amanbharti0302 <amanbharti0302@gmail.com>
2021-02-26 02:42:33 +0530

    latetst

--- soln.cpp
+++ soln.cpp
@@ -1,4 +1,4 @@
-/*......... your happiness is my motivation ............*/
+/*......... my motivation ............*/
 //Coded by Aman Bharti (ECE 2k18) Nit Patna
 #pragma GCC optimize("Ofast", "unroll-loops", "no-stack-protector")
 #include<bits/stdc++.h>
